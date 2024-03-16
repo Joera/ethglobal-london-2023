cc2ef2a29f44ca889ab285ef854f6134ce0fd076
Joera Mulders <joera@pop-os.localdomain>
2024-03-16 08:14:27 +0100

    temporary types

--- fluence/src/services/memTypes/lib.rs
+++ fluence/src/services/memTypes/lib.rs
@@ -3,17 +3,32 @@ use serde_json::{Value};
 use std::collections::BTreeMap;
 use marine_rs_sdk::marine;
 
+#[marine] 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MemMemberAssets { 
+    pub logo: String
+    pub background_color: String
+    pub graph_color: String
+}
 
 #[marine] 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct memSubscription {
+pub struct MemPlan {
+    pub dune_query_id: String
+    pub timebase: String // weekly / monthly / milestone
+    pub milestone: String
+    pub runs_untill: String
+}
+
+#[marine] 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MemSubscription:
     pub name: String
-    pub duneExecutionId: String
-}
+    pub assets: MemMemberAssets
+    pub plan:  MemPlan
 
 #[marine] 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct memContentObject {
-    pub memberName: String
-    pub data: String
-}
+pub struct MemContentObject:
+    pub subscription: MemSubscription
+    pub data_object: String
